765b71f43cba243887d09eaea6fb90d9c880d2c2
Diego Prens Ramos <armandorp10@gmail.com>
2017-09-30 01:38:05 -0500

    Paso 2: Se agrega una prueba utilizando un numero diferente(rojo)

--- calculadoraTest.py
+++ calculadoraTest.py
@@ -8,3 +8,7 @@ class CalculadoraTest(TestCase):
 
     def test_suma_unacadena(self):
         self.assertEquals(Calculadora().suma("1"),1,"Un numero")
+
+    def test_suma_unacadena(self):
+        self.assertEquals(Calculadora().suma("1"),1,"Un numero")
+        self.assertEquals(Calculadora().suma("2"),2,"Un numero")
