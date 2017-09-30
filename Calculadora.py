c24ccbf4e90131b196b57f17e21b53f01e9202ae
Diego Prens Ramos <armandorp10@gmail.com>
2017-09-30 01:48:52 -0500

    Paso 3: Se modifica el codigo para probar test con dos numeros(Verde)

--- Calculadora.py
+++ Calculadora.py
@@ -3,5 +3,7 @@ class Calculadora:
     def suma(self, cadena):
         if cadena == "":
             return 0
+        elif "," in cadena:
+            return int(cadena[0]) + int(cadena[2])
         else:
             return int(cadena)
